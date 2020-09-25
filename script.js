1a9a9b50fdd87314fe24ab1d09537dcbb9a2fd80
williamowen65 <william.owen.bsc@gmail.com>
2020-09-25 14:00:03 -0700

    got a handle on JS DOM

---
+++ script.js
@@ -0,0 +1,23 @@
+const contact = document.querySelector('.contact');
+const spacer = document.querySelector('.spacer');
+const originalsBTN = document.querySelector('.originalsBTN');
+const printsBTN = document.querySelector('.printsBTN');
+const originals = document.querySelector('.originals');
+const prints = document.querySelector('.prints');
+const hidden = document.querySelector('.hidden');
+const flowersObtn = document.querySelector('.flowers-O');
+const animalsPeopleObtn = document.querySelector('.animals-people-O');
+const placesThingsObtn = document.querySelector('.places-things-O');
+const flowersPbtn = document.querySelector('.flowers-P');
+const animalsPeoplePbtn = document.querySelector('.animals-people-P');
+const placesThingsPbtn = document.querySelector('.places-things-P');
+
+const originalFlowers = document.querySelector('.originalFlowers');
+const originalAnimals = document.querySelector('.originalAnimals');
+const originalPlaces = document.querySelector('.originalPlaces');
+const printFlowers = document.querySelector('.printFlowers');
+const printAnimals = document.querySelector('.printAnimals');
+const printPlaces = document.querySelector('.printPlaces');
+
+
+console.log(spacer, originalsBTN, printsBTN, originals, hidden, animalsPeople);
