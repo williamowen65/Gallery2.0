954c6a5ef27f0d7de856b44142fac694f7abe45d
williamowen65 <william.owen.bsc@gmail.com>
2020-09-26 10:07:25 -0700

    ALL or mostly done

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const hidden = document.querySelectorAll('.hidden');
 const homeBTN = document.querySelector('.homeBTN');
 const originalsBTN = document.querySelector('.originalsBTN');
 const printsBTN = document.querySelector('.printsBTN');
+const homeBTNDD = document.querySelector('.homeBTNDD');
+const originalsBTNDD = document.querySelector('.originalsBTNDD');
+const printsBTNDD = document.querySelector('.printsBTNDD');
 const originals = document.querySelector('.originals');
 const prints = document.querySelector('.prints');
 const flowersObtn = document.querySelector('.flowers-O');
@@ -18,71 +21,275 @@ const originalPlaces = document.querySelector('.originalPlaces');
 const printFlowers = document.querySelector('.printFlowers');
 const printAnimals = document.querySelector('.printAnimals');
 const printPlaces = document.querySelector('.printPlaces');
+const hamburger = document.querySelector('.hamburger');
+const mobileNav = document.querySelector('.mobile-nav');
 
 
-console.log(printAnimals);
-console.log(printPlaces);
+hamburger.addEventListener('click', () => {
+ 
+    originals.classList.add('hidden');
+    prints.classList.add('hidden');
+    flowersObtn.classList.add('hidden');
+    animalsPeopleObtn.classList.add('hidden');
+    placesThingsObtn.classList.add('hidden');
+    flowersPbtn.classList.add('hidden');
+    animalsPeoplePbtn.classList.add('hidden');
+    placesThingsPbtn.classList.add('hidden');
+    originalFlowers.classList.add('hidden');
+    originalAnimals.classList.add('hidden');
+    originalPlaces.classList.add('hidden');
+    printFlowers.classList.add('hidden');
+    printAnimals.classList.add('hidden');
+    printPlaces.classList.add('hidden');
+   
+
+    if(!spacer.classList.contains('hidden')){
+        spacer.classList.add('hidden');
+        mobileNav.classList.add('hidden');
+    } else {
+        spacer.classList.remove('hidden');
+        mobileNav.classList.remove('hidden');
+    }
+});
+
+
+homeBTNDD.addEventListener('click', () => {
+    originals.classList.add('hidden');
+    prints.classList.add('hidden');
+    flowersObtn.classList.add('hidden');
+    animalsPeopleObtn.classList.add('hidden');
+    placesThingsObtn.classList.add('hidden');
+    flowersPbtn.classList.add('hidden');
+    animalsPeoplePbtn.classList.add('hidden');
+    placesThingsPbtn.classList.add('hidden');
+    originalFlowers.classList.add('hidden');
+    originalAnimals.classList.add('hidden');
+    originalPlaces.classList.add('hidden');
+    printFlowers.classList.add('hidden');
+    printAnimals.classList.add('hidden');
+    printPlaces.classList.add('hidden');
+    spacer.classList.add('hidden');
+    mobileNav.classList.add('hidden');
+    
+});
+
+originalsBTNDD.addEventListener('click', () => {
+    prints.classList.add('hidden');
+    flowersPbtn.classList.add('hidden');
+    animalsPeoplePbtn.classList.add('hidden');
+    placesThingsPbtn.classList.add('hidden');
+    originalFlowers.classList.add('hidden');
+    originalAnimals.classList.add('hidden');
+    originalPlaces.classList.add('hidden');
+    printFlowers.classList.add('hidden');
+    printAnimals.classList.add('hidden');
+    printPlaces.classList.add('hidden');
+    spacer.classList.add('hidden');
+    mobileNav.classList.add('hidden');
+
+    originals.classList.remove('hidden');
+    flowersObtn.classList.remove('hidden');
+    animalsPeopleObtn.classList.remove('hidden');
+    placesThingsObtn.classList.remove('hidden');
+});
+
+printsBTNDD.addEventListener('click', () => {
+    originals.classList.add('hidden');
+    flowersObtn.classList.add('hidden');
+    animalsPeopleObtn.classList.add('hidden');
+    placesThingsObtn.classList.add('hidden');
+    originalFlowers.classList.add('hidden');
+    originalAnimals.classList.add('hidden');
+    originalPlaces.classList.add('hidden');
+    printFlowers.classList.add('hidden');
+    printAnimals.classList.add('hidden');
+    printPlaces.classList.add('hidden');
+    spacer.classList.add('hidden');
+    mobileNav.classList.add('hidden');
+
+    prints.classList.remove('hidden');
+    flowersPbtn.classList.remove('hidden');
+    animalsPeoplePbtn.classList.remove('hidden');
+    placesThingsPbtn.classList.remove('hidden');
+
+});
+
 
 
 homeBTN.addEventListener('click', () => {
-    hidden.forEach(object => {
-        object.classList.add('hidden');
-    });
-   
+    originals.classList.add('hidden');
+    prints.classList.add('hidden');
+    flowersObtn.classList.add('hidden');
+    animalsPeopleObtn.classList.add('hidden');
+    placesThingsObtn.classList.add('hidden');
+    flowersPbtn.classList.add('hidden');
+    animalsPeoplePbtn.classList.add('hidden');
+    placesThingsPbtn.classList.add('hidden');
+    originalFlowers.classList.add('hidden');
+    originalAnimals.classList.add('hidden');
+    originalPlaces.classList.add('hidden');
+    printFlowers.classList.add('hidden');
+    printAnimals.classList.add('hidden');
+    printPlaces.classList.add('hidden');
+    spacer.classList.add('hidden');
+    mobileNav.classList.add('hidden');
+    
 });
 
 originalsBTN.addEventListener('click', () => {
-    hidden.forEach(object => {
-        object.classList.add('hidden');
-    });
+    prints.classList.add('hidden');
+    flowersPbtn.classList.add('hidden');
+    animalsPeoplePbtn.classList.add('hidden');
+    placesThingsPbtn.classList.add('hidden');
+    originalFlowers.classList.add('hidden');
+    originalAnimals.classList.add('hidden');
+    originalPlaces.classList.add('hidden');
+    printFlowers.classList.add('hidden');
+    printAnimals.classList.add('hidden');
+    printPlaces.classList.add('hidden');
+    spacer.classList.add('hidden');
+    mobileNav.classList.add('hidden');
+
     originals.classList.remove('hidden');
+    flowersObtn.classList.remove('hidden');
+    animalsPeopleObtn.classList.remove('hidden');
+    placesThingsObtn.classList.remove('hidden');
 });
 
 printsBTN.addEventListener('click', () => {
-    hidden.forEach(object => {
-        object.classList.add('hidden');
-    });
+    originals.classList.add('hidden');
+    flowersObtn.classList.add('hidden');
+    animalsPeopleObtn.classList.add('hidden');
+    placesThingsObtn.classList.add('hidden');
+    originalFlowers.classList.add('hidden');
+    originalAnimals.classList.add('hidden');
+    originalPlaces.classList.add('hidden');
+    printFlowers.classList.add('hidden');
+    printAnimals.classList.add('hidden');
+    printPlaces.classList.add('hidden');
+    spacer.classList.add('hidden');
+    mobileNav.classList.add('hidden');
+
     prints.classList.remove('hidden');
+    flowersPbtn.classList.remove('hidden');
+    animalsPeoplePbtn.classList.remove('hidden');
+    placesThingsPbtn.classList.remove('hidden');
+
 });
 
+
+
+
+
+
 flowersObtn.addEventListener('click', () => {
-    hidden.forEach(object => {
-        object.classList.add('hidden');
-    });
+    originals.classList.add('hidden');
+    prints.classList.add('hidden');
+    flowersPbtn.classList.add('hidden');
+    animalsPeoplePbtn.classList.add('hidden');
+    placesThingsPbtn.classList.add('hidden');
+    flowersObtn.classList.add('hidden');
+    animalsPeopleObtn.classList.add('hidden');
+    placesThingsObtn.classList.add('hidden');
+    originalAnimals.classList.add('hidden');
+    originalPlaces.classList.add('hidden');
+    printFlowers.classList.add('hidden');
+    printAnimals.classList.add('hidden');
+    printPlaces.classList.add('hidden');
+    spacer.classList.add('hidden');
+    mobileNav.classList.add('hidden');
+
     originalFlowers.classList.remove('hidden');
 });
 
 animalsPeopleObtn.addEventListener('click', () => {
-    hidden.forEach(object => {
-        object.classList.add('hidden');
-    });
+    originals.classList.add('hidden');
+    prints.classList.add('hidden');
+    flowersPbtn.classList.add('hidden');
+    animalsPeoplePbtn.classList.add('hidden');
+    placesThingsPbtn.classList.add('hidden');
+    flowersObtn.classList.add('hidden');
+    animalsPeopleObtn.classList.add('hidden');
+    placesThingsObtn.classList.add('hidden');
+    originalPlaces.classList.add('hidden');
+    printFlowers.classList.add('hidden');
+    printAnimals.classList.add('hidden');
+    printPlaces.classList.add('hidden');
+    spacer.classList.add('hidden');
+    mobileNav.classList.add('hidden');
+
     originalAnimals.classList.remove('hidden');
 });
 
 placesThingsObtn.addEventListener('click', () => {
-    hidden.forEach(object => {
-        object.classList.add('hidden');
-    });
+    originals.classList.add('hidden');
+    prints.classList.add('hidden');
+    flowersPbtn.classList.add('hidden');
+    animalsPeoplePbtn.classList.add('hidden');
+    placesThingsPbtn.classList.add('hidden');
+    flowersObtn.classList.add('hidden');
+    animalsPeopleObtn.classList.add('hidden');
+    placesThingsObtn.classList.add('hidden');
+    originalAnimals.classList.add('hidden');
+    printFlowers.classList.add('hidden');
+    printAnimals.classList.add('hidden');
+    printPlaces.classList.add('hidden');
+    spacer.classList.add('hidden');
+    mobileNav.classList.add('hidden');
+
     originalPlaces.classList.remove('hidden');
 });
 flowersPbtn.addEventListener('click', () => {
-    hidden.forEach(object => {
-        object.classList.add('hidden');
-    });
+    originals.classList.add('hidden');
+    prints.classList.add('hidden');
+    flowersPbtn.classList.add('hidden');
+    animalsPeoplePbtn.classList.add('hidden');
+    placesThingsPbtn.classList.add('hidden');
+    flowersObtn.classList.add('hidden');
+    animalsPeopleObtn.classList.add('hidden');
+    placesThingsObtn.classList.add('hidden');
+    originalAnimals.classList.add('hidden');
+    printAnimals.classList.add('hidden');
+    printPlaces.classList.add('hidden');
+    spacer.classList.add('hidden');
+    mobileNav.classList.add('hidden');
+
     printFlowers.classList.remove('hidden');
 });
 
 animalsPeoplePbtn.addEventListener('click', () => {
-    hidden.forEach(object => {
-        object.classList.add('hidden');
-    });
+    originals.classList.add('hidden');
+    prints.classList.add('hidden');
+    flowersPbtn.classList.add('hidden');
+    animalsPeoplePbtn.classList.add('hidden');
+    placesThingsPbtn.classList.add('hidden');
+    flowersObtn.classList.add('hidden');
+    animalsPeopleObtn.classList.add('hidden');
+    placesThingsObtn.classList.add('hidden');
+    originalAnimals.classList.add('hidden');
+    printFlowers.classList.add('hidden');
+    printPlaces.classList.add('hidden');
+    spacer.classList.add('hidden');
+    mobileNav.classList.add('hidden');
+
     printAnimals.classList.remove('hidden');
 });
 
 placesThingsPbtn.addEventListener('click', () => {
-    hidden.forEach(object => {
-        object.classList.add('hidden');
-    });
+    originals.classList.add('hidden');
+    prints.classList.add('hidden');
+    flowersPbtn.classList.add('hidden');
+    animalsPeoplePbtn.classList.add('hidden');
+    placesThingsPbtn.classList.add('hidden');
+    flowersObtn.classList.add('hidden');
+    animalsPeopleObtn.classList.add('hidden');
+    placesThingsObtn.classList.add('hidden');
+    originalAnimals.classList.add('hidden');
+    printFlowers.classList.add('hidden');
+    printAnimals.classList.add('hidden');
+    spacer.classList.add('hidden');
+    mobileNav.classList.add('hidden');
+
     printPlaces.classList.remove('hidden');
 });
 
