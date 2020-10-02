140b05773aa206b45127d9e3f848cc6ff447e418
williamowen65 <william.owen.bsc@gmail.com>
2020-10-02 16:57:24 -0700

    admin headway/footer buffer

--- admin.js
+++ admin.js
@@ -1,7 +1,10 @@
 const originalFlowersContainer = document.querySelector('.originalFlowers');
+const form = document.querySelector('form');
 
 
-console.log(originalFlowersContainer);
+
+
+
 
 
 
@@ -23,10 +26,35 @@ const addImage = (image) => {
 };
 
 db.collection('images').get().then((snapshot) => {
-    // console.log(snapshot.docs[0].data());
+    // console.log(snapshot.docs[1].data());
     snapshot.docs.forEach(doc => {
         addImage(doc.data());
     })
 }).catch(err => {
     console.log(err);
+});
+
+
+
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    const image = {
+        title: form.title.value,
+        category: form.category.value,
+        dimensions: form.dimensions.value,
+        src: form.src.value,
+        divClass: form.divClass.value,
+        spanClass: form.spanClass.value
+    };
+
+    console.log(image);
+
+db.collection('images').add(image).then(() => {
+    console.log('image added');
+    form.clear();
+    }).catch(err => {
+        console.log(err);
+    });
 });
