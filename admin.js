70aa9874b7fe48a919f5635dc586fa12f73e7909
williamowen65 <william.owen.bsc@gmail.com>
2020-10-03 17:13:07 -0700

    most admin good, need fix for only one element edit at time

--- admin.js
+++ admin.js
@@ -90,8 +90,104 @@ const allIMG = document.querySelector('.grid');
 allIMG.addEventListener('dblclick', e => {
     // console.log(e.target.parentElement);
     
+
+    const image = e.target.getAttribute('src');
     const id = e.target.parentElement.getAttribute('data-id');
-    console.log(db.collection('images').doc(id).update({
-       "title": "Made it to Shi Shi"
-    }));
+    const title = e.target.nextElementSibling.firstElementChild.innerText;
+    const dimensions = e.target.nextElementSibling.lastElementChild.innerText;
+    const category = e.target.parentElement.parentElement.classList[0];
+    console.log(dimensions, e);
+    
+   
+
+
+
+    let html = ` <p class="exit">X</p>
+        <img src="${image}" class="demo" data-id="${id}">
+        <div class="newInfo">
+            <label for="newTitle"> Title: </label>
+            <input name="newTitle" type="text" id="newTitle" value="${title}" required>
+        </div>
+        <div class="newInfo">
+            <label for="newImg"> SRC: </label>
+            <input name="newImg" type="text" id="newImg" value="${image}" required>
+        </div>
+        <div class="newInfo">
+            <label for="newDimensions"> dimensions: </label>
+            <input name="newDimensions" type="text" id="newDimensions" value="${dimensions}" required><br>
+            <small>NOTE: Use single quotes twice, not double quotes</small><br>
+           <small> Use <code>&lt;br&gt;</code> between sets of sizes (inserting a break)</small>
+        </div>
+        <div class="newInfo">
+            <label for="newCategory"> category: </label>
+            <input name="newCategory" type="text" id="newCategory" value="${category}" required>
+        </div>
+
+        <input type="submit" class="button" value="send">
+        <br>
+        <h4 class="delete">Delete</h4>
+
+        `;
+
+    const editForm = document.querySelector('.editor');
+    const backdrop = document.querySelector('.backdrop');
+
+    editForm.innerHTML += html;
+    backdrop.setAttribute('style','display: block;');
+
+    /* double click out of box to exit */
+setTimeout(() => {
+    const exit = document.querySelector('.exit');
+    exit.addEventListener('click', e => {
+        // console.log(e.target.offsetParent.className);
+        // console.log(e);
+        
+            // console.log('hi');
+            backdrop.setAttribute('style','display: none;');
+            editForm.innerHTML ='';
+        
+     
+    });
+}, 5);
+
+
+
+const deleteImg = document.querySelector('.delete');
+console.log(deleteImg);
+deleteImg.addEventListener('dblclick', () => {
+    if(confirm('are you sure?')){
+        const id = document.querySelector('img.demo').getAttribute('data-id');
+        db.collection('images').doc(id).delete();
+    };
 });
+
+
+
+});
+
+
+/* EDIT OR DELETE Backend */
+const formPopup = document.querySelector('form.editor');
+
+formPopup.addEventListener('submit', e => {
+    e.preventDefault();
+    // console.log('hi');
+    const titlePop = formPopup.newTitle.value;
+    const srcPop = formPopup.newImg.value;
+    const dimensionsPop = formPopup.newDimensions.value;
+    const categoryPop = formPopup.newCategory.value;
+
+    const id = document.querySelector('img.demo').getAttribute('data-id');
+    console.log(id);
+    db.collection('images').doc(id).update({
+        "title": titlePop,
+        "src": srcPop,
+        "dimensions": dimensionsPop,
+        "category": categoryPop
+    });
+});
+
+
+
+
+ 
